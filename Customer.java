1447d918e2b6f0377768ef9a9d872b64bdcf45a6
iremakova <160011981+iremakova@users.noreply.github.com>
2025-05-02 16:40:09 +0300

    Add files via upload
    Model classes have been added.

---
+++ Customer.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Customer extends User {
+    public Customer(int userID, String username) {
+        super(userID, username);
+    }
+
+
+}
---
+++ Order.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Order {
+    private int orderID;
+    private int bookingID;
+    private int menuID;
+    private String status;
+
+    public Order(int orderID, int bookingID, int menuID, String status) {
+        this.orderID = orderID;
+        this.bookingID = bookingID;
+        this.menuID = menuID;
+        this.status = status;
+    }
+
+    public int getOrderID() { return orderID; }
+    public int getBookingID() { return bookingID; }
+    public int getMenuID() { return menuID; }
+    public String getStatus() { return status; }
+}
