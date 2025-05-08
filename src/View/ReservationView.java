24dd0dd40c5d7da943597bafb981d00f04ef2ced
iremakova <irem.akova@ozu.edu.tr>
2025-05-09 01:20:57 +0300

    Reservation controller view have been updated.

---
+++ src/View/ReservationView.java
@@ -0,0 +1,83 @@
+package View;
+
+import Model.User;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ReservationView extends View {
+    private JFrame frame;
+    private JComboBox<String> tableSlotComboBox;
+    private JButton reserveButton;
+    private JButton viewReservationsButton;
+
+
+    public ReservationView() {
+        super("Make Reservation");
+
+        frame = new JFrame(viewTitle);
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel(new GridLayout(2, 1));
+        tableSlotComboBox = new JComboBox<>();
+        reserveButton = new JButton("Reserve");
+        viewReservationsButton = new JButton("View My Reservations");
+
+        panel.add(new JLabel("Select Table & Time Slot:"));
+        panel.add(tableSlotComboBox);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(reserveButton);
+        buttonPanel.add(viewReservationsButton);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.add(panel, BorderLayout.CENTER);
+
+    }
+
+    public JComboBox<String> getTableSlotComboBox() {
+        return tableSlotComboBox;
+    }
+
+    public JButton getReserveButton() {
+        return reserveButton;
+    }
+
+    // === ViewInterface Methods ===
+
+    @Override
+    public void show() {
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void updateView(Object data) {
+        if (data instanceof String) {
+            JOptionPane.showMessageDialog(frame, (String) data);
+        }
+    }
+
+
+    @Override
+    public void reset() {
+        tableSlotComboBox.removeAllItems();
+    }
+
+    @Override
+    public String getViewName() {
+        return "ReservationView";
+    }
+    @Override
+    public void setUser(User user) {
+        // If you're not using user data, this can stay empty
+        // Or you can store it like:
+        // this.user = user;
+    }
+
+    public JButton getViewReservationsButton() {
+        return viewReservationsButton;
+    }
+
+
+}
